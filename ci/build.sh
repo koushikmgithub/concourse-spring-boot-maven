61744d6e35bda1903290025cbab7d52a6d6b2d11
Koushik Maiti <koushik.maiti@yahoo.com>
2020-02-11 00:27:42 +0000

    commit

--- ci/build.sh
+++ ci/build.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e -u -x
 
 cd source-code
   #ls ./src/main/resources/static
   #cp -a ./react-src/build ./src/main/resources/static
-  ./mvnw clean package
+./mvnw clean package
 cd ..
 
 cp source-code/target/concourse-spring-boot-maven-0.0.1-SNAPSHOT.jar  build-output/.
